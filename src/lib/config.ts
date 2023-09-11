d842a61d1191126a2ac3e4d7e36a02a618c9b388
Redon Emini <redonemini18@gmail.com>
2023-09-11 05:00:04 +0200

    first commit - add main pages + styles

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from '$app/environment'
+
+export const title = "DevLog"
+export const description = "Blog app for developers"
+
+export const url = dev ? 'http://localhost:5173/' : 'https://devlog.io/'
